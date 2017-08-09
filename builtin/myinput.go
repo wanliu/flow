6c565d38381d159d0f2f4b20a3d60ff43faa059b
liulei <satbirdd@.com>
2017-08-09 11:38:08 +0800

    seperate input and merge

--- builtin/myinput.go
+++ builtin/myinput.go
@@ -1,12 +1,9 @@
 package builtin
 
 import (
-	"log"
 	"strings"
 
-	"github.com/hysios/apiai-go"
 	. "github.com/wanliu/flow/context"
-	goflow "github.com/wanliu/goflow"
 )
 
 type MyInput struct {
@@ -18,18 +15,6 @@ type MyInput struct {
 
 func NewMyInput() interface{} {
 	return new(MyInput)
-}
-
-func NewQuerySave() interface{} {
-	return new(QuerySave)
-}
-
-type QuerySave struct {
-	goflow.Component
-	MultiField
-	Result <-chan apiai.Result
-	Ctx    <-chan Context
-	Out    chan<- Context
 }
 
 func (in *MyInput) Loop() {
@@ -51,33 +36,4 @@ func (in *MyInput) Loop() {
 			in.SetPrompt(prompt)
 		}
 	}
-}
-
-func (q *QuerySave) Init() {
-	q.Fields = []string{"Ctx", "Result"}
-	q.Process = func() error {
-		res, rok := q.Value("Result").(apiai.Result)
-		ctx, cok := q.Value("Ctx").(Context)
-		if rok && cok {
-			ctx.SetValue("Result", res)
-			intent := res.Metadata.IntentName
-			score := res.Score
-			query := res.ResolvedQuery
-
-			log.Printf("意图解析\"%s\" -> %s 准确度: %2.2f%%", query, intent, score*100)
-			go func() {
-				q.Out <- ctx
-			}()
-		}
-
-		return nil
-	}
-}
-
-func (q *QuerySave) OnCtx(ctx Context) {
-	q.SetValue("Ctx", ctx)
-}
-
-func (q *QuerySave) OnResult(res apiai.Result) {
-	q.SetValue("Result", res)
 }
