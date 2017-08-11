10af58c45a2e468cc8c6e4c974d9846228f5775f
liulei <satbirdd@.com>
2017-08-11 16:24:27 +0800

    set delay reply on graph

--- builtin/final.go
+++ builtin/final.go
@@ -2,7 +2,9 @@ package builtin
 
 import (
 	"github.com/oleiade/lane"
+	"log"
 	"math/rand"
+	"strconv"
 	"sync"
 	"time"
 
@@ -20,7 +22,12 @@ type Final struct {
 
 	ReplyQueue *lane.Queue
 
-	In <-chan ReplyData
+	delayMin int
+	delayMax int
+
+	In       <-chan ReplyData
+	DelayMin <-chan float64
+	DelayMax <-chan float64
 }
 
 func (s *Final) Init() {
@@ -35,14 +42,38 @@ func (s *Final) OnIn(data ReplyData) {
 	s.SendReply()
 }
 
-func (s *Final) SendReply() {
+func (s *Final) OnDelayMin(min float64) {
+	log.Printf("-------------Min: %v", min)
+	s.delayMin = int(min)
+}
+
+func (s *Final) OnDelayMax(max float64) {
+	log.Printf("-------------Max: %v", max)
+	s.delayMax = int(max)
+}
+
+func (s Final) DelayRange() int {
 	rand.Seed(time.Now().UnixNano())
+
+	if s.delayMin == 0 {
+		return 3 + rand.Intn(3)
+	} else {
+		if s.delayMax > s.delayMin {
+			return s.delayMin + rand.Intn(s.delayMax-s.delayMin)
+		} else {
+			return s.delayMin + rand.Intn(3)
+		}
+	}
+}
+
+func (s *Final) SendReply() {
+	secs := s.DelayRange()
 
 	s.RLock()
 	for s.ReplyQueue.Head() != nil {
 		data := s.ReplyQueue.Dequeue().(ReplyData)
 
-		secs := 3 + rand.Intn(3)
+		log.Printf("[Delay]Delay reply for " + strconv.Itoa(secs) + " seconds.")
 		time.Sleep(time.Second * time.Duration(secs))
 
 		data.Ctx.Post(data.Reply)
