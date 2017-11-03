192431e5f004435a16347e4b46fc010c68aeeb4f
liulei <satbirdd@.com>
2017-11-03 15:42:37 +0800

    capacity

--- builtin/text_preprocesor.go
+++ builtin/text_preprocesor.go
@@ -8,6 +8,19 @@ import (
 	flow "github.com/wanliu/goflow"
 )
 
+var REPLACE_DICT map[string]string = map[string]string{
+	"今早":   "今天早上",
+	"今下":   "今天下午",
+	"明早":   "明天早上",
+	"明下":   "明天下午",
+	"今早上":  "今天早上",
+	"今下午":  "今天下午",
+	"明早上":  "明天早上",
+	"明下午":  "明天下午",
+	"1.1红": "1100红",
+	"1.1原": "1100原",
+}
+
 type TextPreprocesor struct {
 	flow.Component
 
@@ -24,6 +37,7 @@ func NewTextPreprocesor() interface{} {
 func (c *TextPreprocesor) OnIn(input string) {
 	output := numberAfterLetter(input)
 	output = dateTransfer(output)
+	output = dictTransfer(output)
 	c.Out <- output
 }
 
@@ -53,15 +67,6 @@ func numberAfterLetter(s string) string {
 // 后天
 // [星期日,星期天,周日]
 func dateTransfer(s string) string {
-	s = strings.Replace(s, "今早", "今天早上", -1)
-	s = strings.Replace(s, "今下", "今天下午", -1)
-	s = strings.Replace(s, "明早", "明天早上", -1)
-	s = strings.Replace(s, "明下", "明天下午", -1)
-	s = strings.Replace(s, "今早上", "今天早上", -1)
-	s = strings.Replace(s, "今下午", "今天下午", -1)
-	s = strings.Replace(s, "明早上", "明天早上", -1)
-	s = strings.Replace(s, "明下午", "明天下午", -1)
-
 	dat := time.Now().AddDate(0, 0, 2)
 	datStr := dat.Format("1月2日")
 	s = strings.Replace(s, "后天", datStr, -1)
@@ -77,6 +82,14 @@ func dateTransfer(s string) string {
 	s = strings.Replace(s, "星期日", datStr, -1)
 	s = strings.Replace(s, "星期天", datStr, -1)
 	s = strings.Replace(s, "周日", datStr, -1)
+
+	return s
+}
+
+func dictTransfer(s string) string {
+	for k, v := range REPLACE_DICT {
+		s = strings.Replace(s, k, v, -1)
+	}
 
 	return s
 }
