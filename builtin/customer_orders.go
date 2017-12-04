ba3b7d68f2cfda5bea3bf9ef88655156afcbaa33
liulei <satbirdd@.com>
2017-12-04 17:59:12 +0800

    customer orders page

--- builtin/customer_orders.go
+++ builtin/customer_orders.go
@@ -1,12 +1,9 @@
 package builtin
 
 import (
-	"fmt"
-	"time"
-
-	"github.com/hysios/apiai-go"
-	"github.com/wanliu/brain_data/resolves"
 	// "github.com/wanliu/flow/builtin/ai"
+	"github.com/wanliu/flow/builtin/config"
+	"github.com/wanliu/flow/builtin/resolves"
 	"github.com/wanliu/flow/context"
 )
 
@@ -16,9 +13,12 @@ const (
 
 type CustomerOrders struct {
 	TryGetEntities
-	Ctx  <-chan context.Context
 	Type <-chan string
-	Out  chan<- ReplyData
+
+	Ctx  <-chan context.Context
+	Page <-chan context.Context
+
+	Out chan<- ReplyData
 }
 
 func NewCustomerOrders() interface{} {
@@ -26,11 +26,26 @@ func NewCustomerOrders() interface{} {
 }
 
 func (c *CustomerOrders) OnCtx(ctx context.Context) {
-	rsv := resolves.NewCusOrdersResolve(ctx)
-
-	rsv.Setup(ctx)
+	rsv := resolves.NewCusOrdersResolve(ctx, Per)
 
 	reply := rsv.Answer()
 
+	rsv.Setup(ctx)
+
 	c.Out <- ReplyData{reply, ctx}
+}
+
+func (c *CustomerOrders) OnPage(ctx context.Context) {
+	in := ctx.Value(config.CtxKeyCusOrders)
+	if in == nil {
+		c.Out <- ReplyData{"当前没有正在进行的查询", ctx}
+	} else {
+		rsv := in.(*resolves.CustomerOrdersResolve)
+		reply := rsv.Answer()
+		c.Out <- ReplyData{reply, ctx}
+
+		if rsv.Done {
+			rsv.Clear(ctx)
+		}
+	}
 }
--- builtin/resolves/customer_orders.go
+++ builtin/resolves/customer_orders.go
@@ -1,32 +1,42 @@
 package resolves
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/hysios/apiai-go"
-	"github.com/wanliu/brain_data/config"
 	"github.com/wanliu/brain_data/database"
 	"github.com/wanliu/flow/builtin/ai"
+	"github.com/wanliu/flow/builtin/config"
 	"github.com/wanliu/flow/context"
 )
 
 type CustomerOrdersResolve struct {
-	Total    int
-	CursorId *uint
-	Done     bool
-	Viewed   int
+	Total      int
+	CursorId   *uint
+	Done       bool
+	Prefetched int
+	Fetched    int
+	Per        int
 
-	Customer string
-	Count    int
-	BeginT   *time.Time
-	EndT     *time.Time
+	CustomerName string
+	Customer     *database.People
+	Count        int
+	QuertyTime   time.Time
+	Duration     string
+	BeginT       *time.Time
+	EndT         *time.Time
 }
 
-func NewCusOrderResolve(ctx context.Context) *CustomerOrdersResolve {
+func NewCusOrdersResolve(ctx context.Context, perPage int) *CustomerOrdersResolve {
+	if perPage <= 0 {
+		perPage = 5
+	}
+
 	aiResult := ctx.Value("Result").(apiai.Result)
 	aiParams := ai.ApiAiOrder{AiResult: aiResult}
 
-	customer := aiParams.Customer()
+	customerName := aiParams.Customer()
 	queryTime := aiParams.Time()
 	count := aiParams.Count()
 	duration := aiParams.Duration()
@@ -34,119 +44,161 @@ func NewCusOrderResolve(ctx context.Context) *CustomerOrdersResolve {
 	beginT, endT := getBeginAndEndTime(duration, queryTime)
 
 	rsv := CustomerOrdersResolve{
-		Customer: customer,
-		BeginT:   beginT,
-		EndT:     endT,
-		Count:    count,
-	}
-
-	if customer == "" {
-		rsv.Done == true
+		CustomerName: customerName,
+		Duration:     duration,
+		QuertyTime:   queryTime,
+		BeginT:       beginT,
+		EndT:         endT,
+		Count:        count,
+		Per:          perPage,
+		Total:        -1,
 	}
 
 	return &rsv
 }
 
 func (r *CustomerOrdersResolve) Answer() string {
-	if r.Customer == "" {
+	if r.CustomerName == "" {
+		r.Done = true
 		return "未提供客户，无法查询客户订单"
 	}
 
-	// fmt.Printf("[NUMBER]---->%v", count)
+	if r.Done {
+		return "查询的订单已经显示完毕"
+	}
 
-	// if count == 0 {
-	// 	count = 2
-	// }
+	r.FetchCustomer()
 
-	// if count > 5 {
-	// 	count = 5
-	// }
+	if r.Customer == nil {
+		r.Done = true
+		return fmt.Sprintf("客户\"%v\"不存在，无法查询客户订单", r.CustomerName)
+	}
 
-	// if customer == "" {
-	// 	c.Out <- ReplyData{"请提供要查询的客户", ctx}
-	// 	return
-	// }
+	result := r.AnswerHeader()
 
-	// var person database.People
-	// err := database.DB.Where("name = ?", customer).First(&person).Error
-	// if nil != err || database.DB.NewRecord(person) {
-	// 	c.Out <- ReplyData{fmt.Sprintf("客户\"%v\"不存在", customer), ctx}
-	// 	return
-	// }
+	if r.Total == -1 {
+		r.FetchTotal()
+	}
 
-	// var orders []database.Order
-	// result := ""
+	if r.Total == 0 {
+		result = result + "没有订单可以显示"
+		return result
+	}
 
-	// if queryTime.IsZero() {
-	// 	person.GetRecentOrders(&orders, nil, nil, count)
-	// 	if len(orders) == 0 {
-	// 		reply := fmt.Sprintf("客户\"%v\"最近没有订单", customer)
-	// 		c.Out <- ReplyData{reply, ctx}
-	// 		return
-	// 	}
+	orders := r.FetchOrders()
+	result = result + fmt.Sprintf("共%v个订单，以下为第%v到第%v个：\n", r.Total, r.Prefetched+1, r.Fetched)
+	r.Done = r.IsDone()
 
-	// 	if count > len(orders) {
-	// 		result = fmt.Sprintf("客户\"%v\"只有%v个订单：\n", customer, len(orders))
-	// 	} else {
-	// 		result = fmt.Sprintf("客户\"%v\"最近的%v个订单：\n", customer, len(orders))
-	// 	}
-	// } else {
-	// 	person.GetRecentOrders(&orders, &queryTime, nil, count)
-	// 	date := queryTime.Format("2006年01月02日")
+	if orders != nil && len(*orders) > 0 {
+		for _, order := range *orders {
+			result = result + "------------------------\n"
+			result = result + fmt.Sprintf("订单号：%v\n总金额：%v\n送货时间：%v\n", order.No, order.Amount, order.DeliveryTime.Format("2006年01月02日"))
+			if order.Note != "" {
+				result = result + fmt.Sprint("备注：%v\n", order.Note)
+			}
 
-	// 	if len(orders) == 0 {
-	// 		reply := fmt.Sprintf("客户\"%v\"在%v没有订单", customer, date)
-	// 		c.Out <- ReplyData{reply, ctx}
-	// 		return
-	// 	}
+			result = result + "商品:\n"
+			for _, item := range order.OrderItems {
+				result = result + fmt.Sprintf("  %v %v%v\n", item.ProductName, item.Quantity, item.Unit)
+			}
 
-	// 	if count > len(orders) {
-	// 		result = fmt.Sprintf("客户\"%v\"%v只有%v个订单：\n", customer, date, len(orders))
-	// 	} else {
-	// 		result = fmt.Sprintf("客户\"%v\"%v最近的%v个订单：\n", customer, date, len(orders))
-	// 	}
-	// }
+			if len(order.GiftItems) > 0 {
+				result = result + "赠品:\n"
+				for _, gift := range order.GiftItems {
+					result = result + fmt.Sprintf("  %v %v%v\n", gift.ProductName, gift.Quantity, gift.Unit)
+				}
+			}
+		}
 
-	// for _, order := range orders {
-	// 	result = result + "------------------------\n"
-	// 	result = result + fmt.Sprintf("订单号：%v\n总金额：%v\n送货时间：%v\n", order.No, order.Amount, order.DeliveryTime.Format("2006年01月02日"))
-	// 	if order.Note != "" {
-	// 		result = result + fmt.Sprint("备注：%v\n", order.Note)
-	// 	}
+		if !r.Done {
+			result = result + "------------------------\n"
+			result = result + "输入\"继续\"，或者\"下一页\"，查看剩下的订单\n"
+		}
+	} else {
+		result = result + "没有订单可以显示"
+	}
 
-	// 	result = result + "商品:\n"
-	// 	for _, item := range order.OrderItems {
-	// 		result = result + fmt.Sprintf("  %v %v%v\n", item.ProductName, item.Quantity, item.Unit)
-	// 	}
+	return result
+}
 
-	// 	if len(order.GiftItems) > 0 {
-	// 		result = result + "赠品:\n"
-	// 		for _, gift := range order.GiftItems {
-	// 			result = result + fmt.Sprintf("  %v %v%v\n", gift.ProductName, gift.Quantity, gift.Unit)
-	// 		}
-	// 	}
-	// }
+func (r *CustomerOrdersResolve) AnswerHeader() string {
+	if r.Duration != "" {
+		return fmt.Sprintf("查询客户\"%v\"%v的订单\n", r.CustomerName, r.Duration)
+	} else if !r.QuertyTime.IsZero() {
+		return fmt.Sprintf("查询客户\"%v\"%v的订单\n", r.CustomerName, r.QuertyTime.Format("2006年1月2日"))
+	} else {
+		return fmt.Sprintf("查询客户\"%v\"最近的订单\n", r.CustomerName)
+	}
+}
+
+func (r *CustomerOrdersResolve) FetchCustomer() {
+	if r.Customer == nil {
+		r.Customer, _ = database.GetPersonByName(r.CustomerName)
+	}
+}
+
+func (r *CustomerOrdersResolve) FetchTotal() {
+	if r.Customer != nil {
+		r.Total = r.Customer.GetRecentOrdersTotal(r.BeginT, r.EndT)
+	} else {
+		r.Total = 0
+	}
+}
+
+func (r *CustomerOrdersResolve) FetchOrders() *[]database.Order {
+	var orders []database.Order
+
+	if r.Customer == nil {
+		return nil
+	}
+
+	r.Customer.GetRecentOrders(&orders, r.BeginT, r.EndT, r.CursorId, 0, r.Per)
+
+	l := len(orders)
+	if l > 0 {
+		r.Prefetched = r.Fetched
+
+		r.Fetched = r.Fetched + l
+		lastOrder := orders[l-1]
+		lid := lastOrder.ID
+		r.CursorId = &lid
+	}
+
+	return &orders
 }
 
 // A.有指定数目时：
 //   1.订单数大于要求的数目，要求的数目已经达到
 //   2.订单数不足要求的数目，但是所有的订单显示完毕
 // B.无指定数目时: 但是所有的订单显示完毕
-func (r *CustomerOrdersResolve) IsDone() {
+func (r *CustomerOrdersResolve) IsDone() bool {
 	if r.Count != 0 {
 		if r.Total >= r.Count {
-			return r.Viewed >= r.Count
+			return r.Fetched >= r.Count
 		} else {
-			return r.Viewed >= r.Total
+			return r.Fetched >= r.Total
 		}
 	} else {
-		return r.Viewed >= r.Total
+		return r.Fetched >= r.Total
 	}
 }
 
 func (r *CustomerOrdersResolve) Setup(ctx context.Context) {
 	if !r.Done {
-		ctx.Value(config.CtxKeyCusOrders, r)
+		ctx.SetValue(config.CtxKeyCusOrders, r)
+	}
+}
+
+func (r *CustomerOrdersResolve) Clear(ctx context.Context) {
+	if r.Done {
+		in := ctx.Value(config.CtxKeyCusOrders)
+		if in != nil {
+			rsv := in.(*CustomerOrdersResolve)
+
+			if r == rsv {
+				ctx.SetValue(config.CtxKeyCusOrders, nil)
+			}
+		}
 	}
 }
 
@@ -214,10 +266,11 @@ func getTimeFromDuration(duration string) (*time.Time, *time.Time) {
 	case "上周":
 		wt := BeginOfWeek(now)
 		// tt := tt.Add(-7 * 24 * time.Hour)
-		tt := tt.AddDate(0, 0, -7)
+		tt := wt.AddDate(0, 0, -7)
 		bt := BeginOfWeek(tt)
+		et := EndOfWeek(tt)
 
-		return &bt, &now
+		return &bt, &et
 	case "近一个月":
 		// bt := now.Add(-30 * 24 * time.Hour)
 		bt := now.AddDate(0, 0, -30)
@@ -232,36 +285,49 @@ func getTimeFromDuration(duration string) (*time.Time, *time.Time) {
 }
 
 func getBETimeOfMonth(m int) (*time.Time, *time.Time) {
-	year, month, _ := now.Date()
+	var (
+		bt time.Time
+		et time.Time
+	)
 
-	if month >= m {
-		tt := time.Date(year, m, 1, 0, 0, 0, 0, t.Location())
-		bt := BeginOfMonth(tt)
-		et := EndOfMonth(tt)
+	t := time.Now()
+	year, month, _ := t.Date()
+
+	if int(month) >= m {
+		tt := time.Date(year, time.Month(m), 1, 0, 0, 0, 0, t.Location())
+		bt = BeginOfMonth(tt)
+		et = EndOfMonth(tt)
 	} else {
 		year = year - 1
-		tt := time.Date(year, m, 1, 0, 0, 0, 0, t.Location())
-		bt := BeginOfMonth(tt)
-		et := EndOfMonth(tt)
+		tt := time.Date(year, time.Month(m), 1, 0, 0, 0, 0, t.Location())
+		bt = BeginOfMonth(tt)
+		et = EndOfMonth(tt)
 	}
 
 	return &bt, &et
 }
 
 func BeginOfMonth(t time.Time) time.Time {
-	year, month, day := t.Date()
-	return time.Date(year, month, 0, 0, 0, 0, 0, t.Location())
+	year, month, _ := t.Date()
+	return time.Date(year, month, 1, 0, 0, 0, 0, t.Location())
 }
 
 func BeginOfWeek(t time.Time) time.Time {
-	step := int(time.Now().Weekday() - 1)
-	tt := now.Add(-step * 24 * time.Hour)
+	step := int(t.Weekday() - 1)
+	tt := t.AddDate(0, 0, -step)
 
 	return BeginOfDay(tt)
 }
 
+func EndOfWeek(t time.Time) time.Time {
+	step := int(7 - t.Weekday())
+	tt := t.AddDate(0, 0, step)
+
+	return EndOfDay(tt)
+}
+
 func EndOfMonth(t time.Time) time.Time {
-	year, month, day := t.Date()
+	year, month, _ := t.Date()
 
 	if month == 12 {
 		year = year + 1
@@ -270,7 +336,7 @@ func EndOfMonth(t time.Time) time.Time {
 		month = month + 1
 	}
 
-	return time.Date(year, month, 0, 0, 0, 0, 0, t.Location())
+	return time.Date(year, month, 0, 23, 59, 59, 999999999, t.Location())
 }
 
 func BeginOfDay(t time.Time) time.Time {
@@ -280,5 +346,80 @@ func BeginOfDay(t time.Time) time.Time {
 
 func EndOfDay(t time.Time) time.Time {
 	year, month, day := t.Date()
-	return time.Date(year, month, day, 23, 59, 59, 0, t.Location())
+	return time.Date(year, month, day, 23, 59, 59, 999999999, t.Location())
 }
+
+// fmt.Printf("[NUMBER]---->%v", count)
+
+// if count == 0 {
+// 	count = 2
+// }
+
+// if count > 5 {
+// 	count = 5
+// }
+
+// if customer == "" {
+// 	c.Out <- ReplyData{"请提供要查询的客户", ctx}
+// 	return
+// }
+
+// var person database.People
+// err := database.DB.Where("name = ?", customer).First(&person).Error
+// if nil != err || database.DB.NewRecord(person) {
+// 	c.Out <- ReplyData{fmt.Sprintf("客户\"%v\"不存在", customer), ctx}
+// 	return
+// }
+
+// var orders []database.Order
+// result := ""
+
+// if queryTime.IsZero() {
+// 	person.GetRecentOrders(&orders, nil, nil, count)
+// 	if len(orders) == 0 {
+// 		reply := fmt.Sprintf("客户\"%v\"最近没有订单", customer)
+// 		c.Out <- ReplyData{reply, ctx}
+// 		return
+// 	}
+
+// 	if count > len(orders) {
+// 		result = fmt.Sprintf("客户\"%v\"只有%v个订单：\n", customer, len(orders))
+// 	} else {
+// 		result = fmt.Sprintf("客户\"%v\"最近的%v个订单：\n", customer, len(orders))
+// 	}
+// } else {
+// 	person.GetRecentOrders(&orders, &queryTime, nil, count)
+// 	date := queryTime.Format("2006年01月02日")
+
+// 	if len(orders) == 0 {
+// 		reply := fmt.Sprintf("客户\"%v\"在%v没有订单", customer, date)
+// 		c.Out <- ReplyData{reply, ctx}
+// 		return
+// 	}
+
+// 	if count > len(orders) {
+// 		result = fmt.Sprintf("客户\"%v\"%v只有%v个订单：\n", customer, date, len(orders))
+// 	} else {
+// 		result = fmt.Sprintf("客户\"%v\"%v最近的%v个订单：\n", customer, date, len(orders))
+// 	}
+// }
+
+// for _, order := range orders {
+// 	result = result + "------------------------\n"
+// 	result = result + fmt.Sprintf("订单号：%v\n总金额：%v\n送货时间：%v\n", order.No, order.Amount, order.DeliveryTime.Format("2006年01月02日"))
+// 	if order.Note != "" {
+// 		result = result + fmt.Sprint("备注：%v\n", order.Note)
+// 	}
+
+// 	result = result + "商品:\n"
+// 	for _, item := range order.OrderItems {
+// 		result = result + fmt.Sprintf("  %v %v%v\n", item.ProductName, item.Quantity, item.Unit)
+// 	}
+
+// 	if len(order.GiftItems) > 0 {
+// 		result = result + "赠品:\n"
+// 		for _, gift := range order.GiftItems {
+// 			result = result + fmt.Sprintf("  %v %v%v\n", gift.ProductName, gift.Quantity, gift.Unit)
+// 		}
+// 	}
+// }
