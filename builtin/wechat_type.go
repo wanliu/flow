5cc3aad8ded18d834d061d51e1b82414e84ecb93
liulei <satbirdd@.com>
2017-12-21 17:11:01 +0800

    fix roomid

--- builtin/wechat_type.go
+++ builtin/wechat_type.go
@@ -27,18 +27,18 @@ func GroupChat(ctx context.Context) bool {
 	return t == 2
 }
 
-// SendId:"@296b2ba89984ea1f66dd5f6753245376",
-// SenderNickName:"胡义",
-// RoomName:"测试群",
-// RoomID:"@Room<测试群>",
-// TaskId:"528219641",
-// MsgID:"8833967666994845528",
-// MsgType:"1",
-// MessageId:"691086ae-b9ba-47b1-8971-e1c2fc4639cd",
-// ReceiptHandle:"xxxxx",
-// Content:"@客服 在吗",
-// WeixinUin:"528219641",
-// BotName:"客服"
+// {"BotName":"客服",
+// "Content":"这个新版的微信, 容易挂掉",
+// "MessageId":"859f8ba8-4ff7-44ae-8d13-1ed3deedd6a9",
+// "MsgID":"5571466961726285032",
+// "MsgType":"1",
+// "ReceiptHandle":"xxx",
+// "RoomID":"@Room\u003c测试群\u003e",
+// "RoomName":"测试群",
+// "SendId":"@f048968daa9e01bb4fbf98067a21ec32",
+// "SenderNickName":"胡义",
+// "TaskId":"528219641",
+// "WeixinUin":"528219641"}
 func wechatType(ctx context.Context) int {
 	log.Printf("[WECHAT GROUP] BEGIN")
 	data, _ := json.Marshal(ctx.Value("WECHAT_INFO"))
@@ -52,7 +52,7 @@ func wechatType(ctx context.Context) int {
 	} else {
 		message := wechatInfo.(map[string]string)
 
-		rooId, ok := message["RoomId"]
+		rooId, ok := message["RoomID"]
 		if ok && rooId != "" {
 			if bootName, ok := message["BotName"]; ok {
 				content, _ := message["Content"]
